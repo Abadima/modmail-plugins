1d28ebd5b8a964379d6506ee28a22bc9a16d18ad
Abadima <39747270+Abadima@users.noreply.github.com>
2021-07-07 17:18:49 -0400

    Update reports.py

--- reports/reports.py
+++ reports/reports.py
@@ -126,7 +126,7 @@ class ReportUser(commands.Cog):
             )
             embed.add_field(name="Reason", value=reason, inline=False)
             embed.set_footer(text=f"Case {self.current_case}")
-            m: discord.Message = await channel.send("@/everyone",embed=embed)
+            m: discord.Message = await channel.send("@everyone",embed=embed)
             await ctx.author.send(self.message)
             await ctx.message.delete()
             await m.add_reaction("\U00002705")
