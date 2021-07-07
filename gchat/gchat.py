37a2f9179b1f5ffe4e159ab8f645d484a85e8432
Abadima <39747270+Abadima@users.noreply.github.com>
2021-07-07 13:45:08 -0400

    Update gchat.py

--- gchat/gchat.py
+++ gchat/gchat.py
@@ -26,9 +26,10 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'words.txt'))
 
 
 class ChatGames(commands.Cog):
-  """
-  Thanks Neko!
-  """
+    """
+    Thanks Neko!
+    """
+
     def __init__(self, bot):
         self.bot = bot
         self.db = bot.api.get_plugin_partition(self)
