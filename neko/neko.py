6490c992f15f670ec3186a6ad15b96d31ebedbaf
Abadima <39747270+Abadima@users.noreply.github.com>
2021-07-08 14:00:04 -0400

    Update neko.py

--- neko/neko.py
+++ neko/neko.py
@@ -16,7 +16,7 @@ class Nekos(commands.Cog):
     @commands.guild_only()
     @commands.cooldown(1, 10, commands.BucketType.member)
     @commands.bot_has_permissions(embed_links=True)
-    async def neko(self, ctx):
+    async def neko(self, ctx: commands.Context, user: discord.Member):
         """Picture of a Neko!"""
 
         author = ctx.author
