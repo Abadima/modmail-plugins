771000cf2f0e35f41b2bfff21d4a3dd583ca4e8b
Abadima <39747270+Abadima@users.noreply.github.com>
2021-07-08 10:22:11 -0400

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -1,10 +1,5 @@
-import datetime
-import logging
-
+import asyncio, logging, datetime, discord, typing
 logger = logging.getLogger("Modmail")
-
-import discord
-import typing
 from discord.ext import commands
 
 from core import checks
@@ -281,7 +276,164 @@ class Moderation(commands.Cog):
         embed.add_field(name="Reason", value=reason)
         embed.add_field(name="Total Warnings", value=warning)
         return embed
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.OWNER)
+    async def nuke(self, ctx, channel: discord.TextChannel = None):
+        """
+        Nukes (deletes EVERY message in) a channel.
+        You can mention a channel to nuke that one instead.
+        """
+        if channel == None:
+            channel = ctx.channel
+        tot = "this" if channel.id == ctx.channel.id else "that"
+        message = await ctx.send(
+            embed=discord.Embed(
+                title="Are you sure?",
+                description=(
+                    f"This command will delete EVERY SINGLE MESSAGE in {tot} channel!\n"
+                    'If you are sure and responsible about what might happen send "Yes, do as I say!". '
+                    "Otherwise, send anything else to abort.\n"
+                    "**Unexpected bad things might happen if you decide to continue!**"
+                ),
+                color=discord.Color.red(),
+            )
+        )
 
+        def surecheck(m):
+            return m.author == ctx.message.author
+
+        try:
+            sure = await self.bot.wait_for("message", check=surecheck, timeout=30)
+        except asyncio.TimeoutError:
+            await message.edit(
+                embed=discord.Embed(title="Aborted.", color=self.bot.main_color)
+            )
+            ensured = False
+        else:
+            if sure.content == "Yes, do as I say!":
+                ensured = True
+            else:
+                await message.edit(
+                    embed=discord.Embed(title="Aborted.", color=self.bot.main_color)
+                )
+                ensured = False
+        if ensured:
+            case = await self.get_case()
+
+            channel_position = channel.position
+
+            try:
+                new_channel = await channel.clone()
+
+                await new_channel.edit(position=channel_position)
+                await channel.delete()
+            except discord.errors.Forbidden:
+                return await ctx.send(
+                    embed=discord.Embed(
+                        title="Error",
+                        description=f"I don't have enough permissions to nuke {tot} channel.",
+                        color=discord.Color.red(),
+                    ).set_footer(text="Please fix the permissions.")
+                )
+
+            await new_channel.send(
+                embed=discord.Embed(
+                    title="Nuke",
+                    description="This channel has been nuked!",
+                    color=self.bot.main_color,
+                )
+                .set_image(
+                    url="https://cdn.discordapp.com/attachments/600843048724987925/600843407228928011/tenor.gif"
+                )
+                .set_footer(text=f"This is the {case} case.")
+            )
+
+            await self.log(
+                guild=ctx.guild,
+                embed=discord.Embed(
+                    title="Nuke",
+                    description=f"{ctx.author.mention} nuked {new_channel.mention}.",
+                    color=self.bot.main_color,
+                ).set_footer(text=f"This is the {case} case."),
+            )
+
+    @commands.command(usage="<amount>")
+    @checks.has_permissions(PermissionLevel.MODERATOR)
+    async def purge(self, ctx, amount: int = 1):
+        """Purge the specified amount of messages."""
+        max = 2000
+        if amount > max:
+            return await ctx.send(
+                embed=discord.Embed(
+                    title="Error",
+                    description=f"You can only purge up to 2000 messages.",
+                    color=discord.Color.red(),
+                ).set_footer(text=f"Use {ctx.prefix}nuke to purge the entire chat.")
+            )
+
+        try:
+            await ctx.message.delete()
+            await ctx.channel.purge(limit=amount)
+        except discord.errors.Forbidden:
+            return await ctx.send(
+                embed=discord.Embed(
+                    title="Error",
+                    description="I don't have enough permissions to purge messages.",
+                    color=discord.Color.red(),
+                ).set_footer(text="Please fix the permissions.")
+            )
+
+        case = await self.get_case()
+        messages = "messages" if amount > 1 else "message"
+        have = "have" if amount > 1 else "has"
+
+        await self.log(
+            guild=ctx.guild,
+            embed=discord.Embed(
+                title="Purge",
+                description=f"{amount} {messages} {have} been purged by {ctx.author.mention}.",
+                color=self.bot.main_color,
+            ).set_footer(text=f"This is the {case} case."),
+        )
+
+        await ctx.send(
+            embed=discord.Embed(
+                title="Success",
+                description=f"Purged {amount} {messages}.",
+                color=self.bot.main_color,
+            ).set_footer(text=f"This is the {case} case.")
+        )
+
+    async def get_case(self):
+        """Gives the case number."""
+        num = await self.db.find_one({"_id": "cases"})
+        if num == None:
+            num = 0
+        elif "amount" in num:
+            num = num["amount"]
+            num = int(num)
+        else:
+            num = 0
+        num += 1
+        await self.db.find_one_and_update(
+            {"_id": "cases"}, {"$set": {"amount": num}}, upsert=True
+        )
+        suffix = ["th", "st", "nd", "rd", "th"][min(num % 10, 4)]
+        if 11 <= (num % 100) <= 13:
+            suffix = "th"
+        return f"{num}{suffix}"
+
+    async def log(self, guild: discord.Guild, embed: discord.Embed):
+        """Sends logs to the log channel."""
+        channel = await self.db.find_one({"_id": "logging"})
+        if channel == None:
+            return
+        if not str(guild.id) in channel:
+            return
+        channel = self.bot.get_channel(channel[str(guild.id)])
+        if channel == None:
+            return
+        return await channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
