72886acdd14f2aa11e464db6acc268cd22b41624
Abadima <39747270+Abadima@users.noreply.github.com>
2021-07-09 00:41:03 -0400

    Update neko.py

--- neko/neko.py
+++ neko/neko.py
@@ -40,6 +40,21 @@ class Nekos(commands.Cog):
         embed.title = f"Neko!~"
         embed.set_image(url=imgjson["url"])
         await ctx.reply(embed=embed)
+        
+    @commands.command(aliases=["ngif"])
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    @commands.cooldown(1, 10, commands.BucketType.member)
+    @commands.bot_has_permissions(embed_links=True)
+    async def nekogif(self, ctx):
+        """Neko Gifs!"""
+        author = ctx.author
+        img = await self.bot.session.get('https://nekos.life/api/v2/img/ngif')
+        imgtxt = await img.text()
+        imgjson = json.loads(imgtxt)
+        embed = discord.Embed(colour=author.colour)
+        embed.title = f"Neko!~"
+        embed.set_image(url=imgjson["url"])
+        await ctx.reply(embed=embed)
 
 def setup(bot):
     bot.add_cog(Nekos(bot))
