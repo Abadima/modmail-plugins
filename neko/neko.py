1e067e27e4402826c51142173ebb492278bae952
Abadima <39747270+Abadima@users.noreply.github.com>
2021-07-11 14:21:45 -0400

    Update neko.py

--- neko/neko.py
+++ neko/neko.py
@@ -22,11 +22,10 @@ class Nekos(commands.Cog):
         result = await self.client.get_image("nekos")
         embed = discord.Embed(
             colour=author.colour,
-            title=f"Neko!~",
-            set_image(url=result.url)
+            title=f"Neko!~"
         )
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-     #   embed.set_image(url=result.url)
+        embed.set_image(url=result.url)
         await ctx.reply(embed=embed)
         
     @commands.command()
