16a2b8dfbcb4e7770ffdccdb9cd30097f392bf8d
Abadima <39747270+Abadima@users.noreply.github.com>
2021-07-13 15:50:20 -0400

    Update neko.py

--- neko/neko.py
+++ neko/neko.py
@@ -85,18 +85,19 @@ class Nekos(commands.Cog):
     @commands.command(aliases=["nav"])
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @commands.cooldown(1, 5, commands.BucketType.member)
-    @commands.bot_has_permissions(embed_links=True)
+ #   @commands.bot_has_permissions(embed_links=True)
     async def nekoavatar(self, ctx):
         """
         Neko Avatar!
         [DISABLED] Too Close to Hentai.
         """
+        
     author = ctx.author
     embed = discord.Embed(
     colour=author.colour,
     title = f"DISABLED.",
-    description = f"This Command has potential NSFW, so to protect our kids, \nwe have disabled this for now."
-).set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+    description = f"This Command has potential NSFW, so to protect our kids, \n we have disabled this for now.",
+).set_author(name=author.name, icon_url=author.avatar_url)
 await ctx.reply(embed=embed)
 
 #       author = ctx.author
