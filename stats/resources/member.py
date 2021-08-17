9b1032969c7159da4970548520e51d00e1d3b75f
Abadima <39747270+Abadima@users.noreply.github.com>
2021-08-17 11:25:03 -0400

    Update member.py

--- stats/resources/member.py
+++ stats/resources/member.py
@@ -69,7 +69,7 @@ class MemberResource:
             activitytype += " to" if activitytype == "Listening" else ""
             embed.add_field(name="Activity", value=f"{activitytype} {m.activity.name}")
             
-        if role_list is not None:
+        if role_list = 0:
             print(role_list)
             embed.add_field(name="Roles", value="Unable to Display Roles")
 
