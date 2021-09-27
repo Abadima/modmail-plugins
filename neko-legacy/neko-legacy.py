b1fbc50345bd47b9c0f1754a86579f80adb8da29
Abadima <39747270+Abadima@users.noreply.github.com>
2021-09-27 18:14:41 -0400

    Update neko-legacy.py

--- neko-legacy/neko-legacy.py
+++ neko-legacy/neko-legacy.py
@@ -81,57 +81,6 @@ class Nekos(commands.Cog):
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         embed.set_image(url=imgjson["url"])
         await ctx.reply(embed=embed)
-        
-    @commands.command(aliases=["nav"])
-    @checks.has_permissions(PermissionLevel.SUPPORTER)
-    @commands.cooldown(1, 5, commands.BucketType.member)
-    @commands.bot_has_permissions(embed_links=True)
-    async def nekoavatar(self, ctx):
-        """
-        Neko Avatar!
-        [DISABLED] Too Close to Hentai.
-        """
-        
-        author = ctx.author
-        embed = discord.Embed(
-        colour=author.colour,
-        title = f"DISABLED.",
-        description = f"This Command has potential NSFW, so to protect our kids, \n we have disabled this for now.",
-        ).set_author(name=author.name, icon_url=author.avatar_url)
-        await ctx.reply(embed=embed)
-
-#       author = ctx.author
-#       img = await self.bot.session.get('https://nekos.life/api/v2/img/avatar')
-#       imgtxt = await img.text()
-#       imgjson = json.loads(imgtxt)
-#       embed = discord.Embed(
-#           colour=author.colour,
-#           title = f"Neko Avatar!"
-#       )
-#       embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-#       embed.set_image(url=imgjson["url"])
-#       await ctx.reply(embed=embed)
-
-    @commands.command(aliases=["tnav"])
-    @checks.has_permissions(PermissionLevel.OWNER)
-    @commands.cooldown(1, 5, commands.BucketType.member)
-    @commands.bot_has_permissions(embed_links=True)
-    async def tnekoavatar(self, ctx):
-        """
-        Neko Avatar!
-        But for Owner Testing
-        """
-        author = ctx.author
-        img = await self.bot.session.get('https://nekos.life/api/v2/img/avatar')
-        imgtxt = await img.text()
-        imgjson = json.loads(imgtxt)
-        embed = discord.Embed(
-           colour=author.colour,
-           title = f"Neko Avatar!"
-       )
-        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        embed.set_image(url=imgjson["url"])
-        await ctx.reply(embed=embed)
 
 def setup(bot):
     bot.add_cog(Nekos(bot))
