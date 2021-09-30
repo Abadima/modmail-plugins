30f944286b76a9c2505f4a3bad79ee5b24b590a9
Abadima <39747270+Abadima@users.noreply.github.com>
2021-09-29 23:45:26 -0400

    Update action.py

--- action/action.py
+++ action/action.py
@@ -87,7 +87,7 @@ class Action(commands.Cog):
             img = await self.bot.session.get('https://v2.yiff.rest/furry/hug')
             imgtxt = await img.text()
             imgjson = json.loads(imgtxt)
-            print(imgjson["[0]"])
+            print(imgjson["images[0]"])
            # print(imgjson[[0].url])
             embed.set_image(url=imgjson["url"])
             return await ctx.reply(embed=embed)
