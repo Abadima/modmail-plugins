e3ed3513721b44fd1a6a73b705d6410702a36549
Abadima <39747270+Abadima@users.noreply.github.com>
2021-09-30 19:26:45 -0400

    Update action.py

--- action/action.py
+++ action/action.py
@@ -111,12 +111,7 @@ class Action(commands.Cog):
             imgtxt = await img.text()
             imgjson = json.loads(imgtxt)
             embed.set_image(url=imgjson["images"][0]["url"])
-            return await ctx.reply(
-                embed=embed,
-                components = [ActionRow(
-                    Button(style=ButtonStyle.link, label="Source", url=imgjson["images"][0]["url"])
-                )])
-                
+            return await ctx.reply(embed=embed)   
         
         if furry_mode is False or None:
             result = await self.client.get_image("hug")
