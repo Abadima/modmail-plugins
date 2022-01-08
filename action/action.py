635c105ec46cbb6f53012227e3b1fd0aea3d2be2
Abadima <39747270+Abadima@users.noreply.github.com>
2022-01-08 15:58:23 -0500

    Update action.py

--- action/action.py
+++ action/action.py
@@ -110,7 +110,8 @@ class Action(commands.Cog):
             img = await self.bot.session.get('https://v2.yiff.rest/furry/hug')
             imgtxt = await img.text()
             imgjson = json.loads(imgtxt)
-            embed.set_image(url=imgjson["images"][0]["url"])
+            print(imgjson["images"][0]["url"])
+           #  embed.set_image(url=imgjson["images"][0]["url"])
             return await ctx.reply(embed=embed)   
         
         if furry_mode is False or None:
